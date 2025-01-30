dd4642b3848275d39d134683d4012fc761e13184
Lincoln Whyte <10820584@uvu.edu>
2025-01-29 23:54:12 -0700

    create scripting 3 lab

---
+++ GameObjectsAndTransformLab/Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.right * 500);
+    }
+
+}
