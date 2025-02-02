fd543177280049be58c7164ddae8005168531045
Lincoln Whyte <10820584@uvu.edu>
2025-02-01 19:19:42 -0700

    add bouncyball and colorchange scripts

---
+++ scripting3lab/Assets/Assets/Scripts/BouncyBall.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class BouncyBall : MonoBehaviour
+{
+    public float bounceForce = 3f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
+    }
+    
+}
